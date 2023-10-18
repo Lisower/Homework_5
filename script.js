7da8e4b52a2b6a318dc6537c32a52ddcc859fc84
Lisower <144545336+Lisower@users.noreply.github.com>
2023-10-18 23:11:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function cost(event){
+    event.preventDefault();
+    type = document.getElementById("tovar-type").value;
+    switch(type){
+        case "tovar-1":
+            price = 10;
+            break;
+        case "tovar-2":
+            price = 20;
+            break;
+        case "tovar-3":
+            price = 30;
+            break;
+        default:
+            price = 10;
+            break;
+    }
+    number = parseInt(document.getElementById("tovar-number").value);
+    if (isNaN(number))
+        if (document.getElementById("tovar-number").value == "") alert("Количество товара не указано!");
+        else alert("Некорректный ввод количества товара!");
+    else
+        document.getElementById("result").innerHTML = "Стоимость заказа: " + price*number + " р. ";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button").addEventListener("click", cost);
+});
